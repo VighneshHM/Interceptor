eb3d7d22b5ad53f914d9a199b264fbeb675fe0af
gautamrajesh007 <gautamrdev7@gmail.com>
2026-01-25 18:43:56 +0530

    add CommandLineRunner to initialize default admin user on startup

--- src/main/java/com/proxy/interceptor/InterceptorApplication.java
+++ src/main/java/com/proxy/interceptor/InterceptorApplication.java
@@ -1,13 +1,28 @@
 package com.proxy.interceptor;
 
+import com.proxy.interceptor.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RequiredArgsConstructor
+@Slf4j
 public class InterceptorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InterceptorApplication.class, args);
+	}
+
+	CommandLineRunner initAdmin(AuthService authService) {
+		return args -> {
+			// Create default admin user if not exists
+			authService.createAdminIfNotExists("admin", "14495abc");
+			log.info("Interceptor Proxy started");
+			log.info("Dashboard: http://localhost:3000");
+		};
 	}
 
 }

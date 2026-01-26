8b8fe119e1ea7735dda2b19579c87e5bd53214c1
gautamrajesh007 <gautamrdev7@gmail.com>
2026-01-26 17:00:37 +0530

    add RedisConfig to configure RedisTemplate and message listeners

---
+++ src/main/java/com/proxy/interceptor/config/RedisConfig.java
@@ -0,0 +1,66 @@
+package com.proxy.interceptor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Key serializers
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+
+        // Value serializers
+        GenericJacksonJsonRedisSerializer serializer =
+                GenericJacksonJsonRedisSerializer
+                        .builder()
+                        .enableUnsafeDefaultTyping()
+                        .build();
+
+        template.setValueSerializer(serializer);
+        template.setHashValueSerializer(serializer);
+        return template;
+    }
+
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(
+            RedisConnectionFactory connectionFactory,
+            MessageListenerAdapter blockedListener,
+            MessageListenerAdapter approvalListener,
+            MessageListenerAdapter voteListener
+    ) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(blockedListener, new PatternTopic("interceptor:blocked"));
+        container.addMessageListener(approvalListener, new PatternTopic("interceptor:approvals"));
+        container.addMessageListener(voteListener, new PatternTopic("interceptor:votes"));
+        return container;
+    }
+
+    @Bean
+    public MessageListenerAdapter blockedListener(RedisMessageHandler handler) {
+        return new MessageListenerAdapter(handler, "handleBlockedMessage");
+    }
+
+    @Bean
+    public MessageListenerAdapter approvalListener(RedisMessageHandler handler) {
+        return new MessageListenerAdapter(handler, "handleApprovalMessage");
+    }
+
+    @Bean
+    public MessageListenerAdapter voteListener(RedisMessageHandler handler) {
+        return new MessageListenerAdapter(handler, "handleVoteMessage");
+    }
+}

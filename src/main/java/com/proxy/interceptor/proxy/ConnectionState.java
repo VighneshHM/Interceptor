b638d32b7cb7009dd8198fa4376061b1022cd1a6
gautamrajesh007 <gautamrdev7@gmail.com>
2026-01-27 10:21:00 +0530

    add resetBatch method to ConnectionState for managing batch state and releasing buffers

--- src/main/java/com/proxy/interceptor/proxy/ConnectionState.java
+++ src/main/java/com/proxy/interceptor/proxy/ConnectionState.java
@@ -1,6 +1,10 @@
 package com.proxy.interceptor.proxy;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ConnectionState {
 
@@ -8,8 +12,21 @@ public class ConnectionState {
     public volatile Channel serverChannel;
     public volatile boolean inExtendedBatch = false;
     public volatile StringBuilder batchQuery = new StringBuilder();
+    public volatile List<ByteBuf> batchBuffers = new ArrayList<>();
 
     public ConnectionState(String connId) {
         this.connId = connId;
+    }
+
+    public void resetBatch() {
+        inExtendedBatch = false;
+        batchQuery = new StringBuilder();
+
+        for (ByteBuf buf: batchBuffers) {
+            if (buf.refCnt() > 0) {
+                buf.release();
+            }
+        }
+        batchBuffers = new ArrayList<>();
     }
 }
